0a126870b893e4501e967aea14bf108ed2d036cc
Akshitha Kattekommula <kakshitha944@gmail.com>
2026-02-19 12:26:02 +0000

    Fixed flake8 issues

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, User, Task
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "supersecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
@@ -16,6 +23,7 @@ login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -35,7 +43,12 @@ def register():
 
         hashed_password = generate_password_hash(password)
 
-        new_user = User(username=username, email=email, password=hashed_password)
+        new_user = User(
+            username=username,
+            email=email,
+            password=hashed_password,
+        )
+
         db.session.add(new_user)
         db.session.commit()
 
@@ -53,7 +66,10 @@ def login():
 
         user = User.query.filter_by(email=email).first()
 
-        if user and check_password_hash(user.password, password):
+        if user and check_password_hash(
+            user.password,
+            password,
+        ):
             login_user(user)
             return redirect(url_for("index"))
 
@@ -70,14 +86,21 @@ def logout():
     return redirect(url_for("login"))
 
 
-# ---------------- TASK ROUTES ----------------
+# ---------------- HOME ----------------
 @app.route("/")
 @login_required
 def index():
-    tasks = Task.query.filter_by(user_id=current_user.id).all()
-    return render_template("index.html", tasks=tasks)
+    tasks = Task.query.filter_by(
+        user_id=current_user.id
+    ).all()
+
+    return render_template(
+        "index.html",
+        tasks=tasks,
+    )
 
 
+# ---------------- ADD TASK ----------------
 @app.route("/add", methods=["GET", "POST"])
 @login_required
 def add_task():
@@ -91,7 +114,7 @@ def add_task():
         new_task = Task(
             title=title,
             description=description,
-            user_id=current_user.id
+            user_id=current_user.id,
         )
 
         db.session.add(new_task)
@@ -102,6 +125,7 @@ def add_task():
     return render_template("add_task.html")
 
 
+# ---------------- DELETE TASK ----------------
 @app.route("/delete/<int:id>")
 @login_required
 def delete_task(id):
@@ -112,9 +136,11 @@ def delete_task(id):
 
     db.session.delete(task)
     db.session.commit()
+
     return redirect(url_for("index"))
 
 
+# ---------------- EDIT TASK ----------------
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit_task(id):
@@ -131,7 +157,10 @@ def edit_task(id):
         db.session.commit()
         return redirect(url_for("index"))
 
-    return render_template("edit_task.html", task=task)
+    return render_template(
+        "edit_task.html",
+        task=task,
+    )
 
 
 if __name__ == "__main__":
