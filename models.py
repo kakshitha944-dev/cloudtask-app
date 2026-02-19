f6a35a1a33456018a21cfe61d101d0e891b0595c
Akshitha Kattekommula <kakshitha944@gmail.com>
2026-02-19 12:36:17 +0000

    Fixed E303 too many blank lines

--- models.py
+++ models.py
@@ -5,14 +5,12 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     tasks = db.relationship('Task', backref='owner', lazy=True)
-
 
 
 class Task(db.Model):
